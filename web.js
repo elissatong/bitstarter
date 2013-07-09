63522eeead8d9ce6528b7b9822712c49a32c463a
Elissa Tong <elissa.tong@gmail.com>
2013-07-09 16:03:39 +0000

    File index.html contents and print it.

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var contents = fs.readFileSync("index.html", "utf8");
+//console.log(contents);
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(contents);
 });
 
 var port = process.env.PORT || 5000;
